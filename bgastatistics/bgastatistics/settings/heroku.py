0bf3082608f76295793fc0ba8be76b57f81c4d1b
Thamara Andrade <tkcandrade@gmail.com>
2021-08-29 23:10:26 -0300

    preparing for heroku

---
+++ bgastatistics/bgastatistics/settings/heroku.py
@@ -0,0 +1,13 @@
+import environ
+
+from bgastatistics.bgastatistics.settings.base import *
+
+env = environ.Env()
+
+# DEBUG = env.bool('DEBUG', default=False)
+DEBUG = True
+SECRET_KEY = env('SECRET_KEY')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+DATABASES = {
+    'default': env.db(),
+}
